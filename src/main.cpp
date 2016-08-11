d46f09bde52991d257fb380f3373d259f150457a
Zev N. Kronenberg <zevk@e64.grid.gs.washington.edu>
2016-08-11 11:28:11 -0700

    calls dels now

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,54 @@
 
 using namespace BamTools;
 
-int main(int argc, char ** argv) {
+//------------------------------------------------------------------------------
+void readInternalPrint(BamAlignment & al,
+                       FastaReference & targetFa,
+                       std::string & refName )
+{
+    std::cerr << al.Name
+              << "\t" << al.Position
+              << "\t" << al.GetEndPosition()
+              << "\t" << al.Length
+              << "\t" << percentID(al.CigarData, al.Length) << std::endl;
+
+    long int qPos = 0;
+    long int rPos = al.Position;
+
+    for(std::vector< CigarOp >::iterator it = al.CigarData.begin();
+        it != al.CigarData.end(); it++){
+
+        if(it->Type == 'I'){
+            std::cout << "INSERTION"
+                      << "\t" << rPos
+                      << "\t" << al.QueryBases.substr(qPos, it->Length)
+                      << std::endl;
+        }
+        if(it->Type == 'D'){
+            std::cout << "DELETION"
+                      << "\t" << rPos
+                      << "\t" << targetFa.getSubSequence(refName,
+                                                         rPos+1, it->Length)
+                      << std::endl;
+        }
+        advanceQuery(it->Type,     it->Length, &qPos);
+        advanceReference(it->Type, it->Length, &rPos);
+    }
+}
+
+//------------------------------------------------------------------------------
+
+int main(int argc, char ** argv)
+{
 
     std::map<std::string, int> inverseLookup;
 
+    if(argc != 4){
+        std::cerr << std::endl;
+        std::cerr << "usage: contigSV your.bam refgenome.fa querygenome.fa"
+                  << std::endl;
+        return(1);
+    }
     /* target is the ref genome */
     /* query is required for inter alignment insertion */
 
@@ -25,13 +69,8 @@ int main(int argc, char ** argv) {
     FastaReference queryFasta  ;
 
     targetFasta.open(argv[2])  ;
-    queryFasta.open(argv[3])   ;
+    queryFasta.open(argv[3] )  ;
 
-    if(argc < 3){
-        std::cerr << std::endl;
-        std::cerr << "usage: contigSV your.bam refgenome.fa querygenome.fa" << std::endl;
-        exit(1);
-    }
 
     std::string bname = argv[1];
 
@@ -39,40 +78,21 @@ int main(int argc, char ** argv) {
 
     if(!br.Open(bname)){
         std::cerr << "FATAL: could not open bam file" << std::endl;
-        exit(1);
+        return(1);
     }
     if(!br.LocateIndex()){
         std::cerr << "FATAL: could not locate bam index" << std::endl;
         std::cerr << "INFO:  check that index is bam.bai" << std::endl;
-        exit(1);
+        return(1);
     }
 
     RefVector rv     = br.GetReferenceData();
-    SamHeader header = br.GetHeader();
 
     BamAlignment al;
 
     while(br.GetNextAlignment(al)){
-
-
-        std::cerr << al.Name << "\t" << al.Position << "\t" << al.GetEndPosition() << "\t" << al.Length << "\t" << percentID(al.CigarData, al.Length) << std::endl;
-
-        long int qPos = 0;
-        long int rPos = al.Position;
-
-        for(std::vector< CigarOp >::iterator it = al.CigarData.begin();
-            it != al.CigarData.end(); it++){
-
-            if(it->Type == 'I'){
-                std::cout << "INSERTION" << "\t" << rPos << "\t" << al.QueryBases.substr(qPos, it->Length) << std::endl;
-            }
-
-            advanceQuery(it->Type, it->Length, &qPos);
-            advanceReference(it->Type, it->Length, &rPos);
-
-
-        }
-
+        readInternalPrint(al, targetFasta, rv[al.RefID].RefName);
     }
 
+    return 0;
 }
