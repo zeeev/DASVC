6ba7d40881fd5586462a2acfa4809a654cb056bc
Zev N. Kronenberg <zevk@e59.grid.gs.washington.edu>
2016-08-15 11:32:07 -0700

    annotation

--- src/annotate.cpp
+++ src/annotate.cpp
@@ -1,12 +1,90 @@
-#include "errors.hpp"
-#include <list>
-#include <string>
+#include "annotate.hpp"
 
-/* bamtools */
-#include "api/BamWriter.h"
-#include "api/BamMultiReader.h"
+bool qStartSort(const BamAlignment L,
+                const BamAlignment R ){
 
-using namespace BamTools;
+    int  lp;
+    int  rp;
+
+    L.GetTag<int>( "QS", lp );
+    R.GetTag<int>( "QS", rp );
+
+    if(lp <= rp){
+        return true;
+    }
+
+    return false;
+}
+
+
+//------------------------------- SUBROUTINE --------------------------------
+/*
+ Function input  : list of BamAlignments
+ Function does   : sorts and orients blocks by query position
+ Function returns: error code
+ Function details: Sorts alignments by first match position then adds
+                   the new optional tag.
+
+*/
+
+int processBlock(std::list< BamAlignment > & readBuffer,
+                 BamWriter & writer, int blockId)
+{
+
+    std::vector<BamAlignment> reads;
+
+    for(std::list<BamAlignment>::iterator it = readBuffer.begin();
+        it != readBuffer.end(); it++){
+
+        long int queryStart = 0;
+
+        for(std::vector<CigarOp>::iterator iz = it->CigarData.begin() ;
+            iz != it->CigarData.end() ; iz++){
+            /* first matching query position */
+            advanceQuery(iz->Type, iz->Length, &queryStart, true);
+            if( (iz->Type != 'H') || (iz->Type != 'S') ){
+                break;
+            }
+        }
+
+        long int queryEnd   = 0;
+        int seen            = 0;
+
+        for(std::vector<CigarOp>::iterator iz = it->CigarData.begin() ;
+            iz != it->CigarData.end() ; iz++){
+            /* first matching query position */
+
+            if( (iz->Type == 'H') || (iz->Type == 'S') ){
+                seen += 1;
+            }
+            if(seen > 1){
+                break;
+            }
+            advanceQuery(iz->Type, iz->Length, &queryEnd, true);
+        }
+
+        double pctID = percentID(it->CigarData, it->Length);
+
+        it->AddTag<int>( "QS", "i", queryStart      );
+        it->AddTag<int>( "QE", "i", queryEnd        );
+        it->AddTag<int>( "QL", "i", (queryEnd - queryStart));
+        it->AddTag<float>( "PI", "f", pctID);
+        it->AddTag<int>( "BI", "i", blockId);
+        it->AddTag<int>( "NB", "i", readBuffer.size());
+
+        reads.push_back(*it);
+    }
+
+    sort(reads.begin(), reads.end(),  qStartSort);
+
+    for(std::list<BamAlignment>::iterator it = readBuffer.begin();
+        it != readBuffer.end(); it++){
+        writer.SaveAlignment(*it);
+    }
+
+    return 0;
+}
+
 
 //------------------------------- SUBROUTINE --------------------------------
 /*
@@ -20,38 +98,55 @@ using namespace BamTools;
 
 int annotate(std::string bfName)
 {
-    BamReader br;
+    BamReader reader;
 
     errorHandler EH;
 
-    if(!br.Open(bfName)){
+    if(!reader.Open(bfName)){
         EH.croak(EH.COULD_NOT_OPEN_BAM, true);
     }
+
+    const SamHeader header     = reader.GetHeader();
+    const RefVector references = reader.GetReferenceData();
+
+    BamWriter writer;
+    if ( !writer.Open("test.out.bam", header, references) ) {
+        EH.croak(EH.COULD_NOT_OPEN_BAM, true);
+    }
+
 
     EH.printInfo("Starting to annotate alignment blocks.");
 
     BamAlignment al;
 
-    if(!br.GetNextAlignment(al)){
+    if(!reader.GetNextAlignment(al)){
         EH.croak(EH.NO_READS, true);
     }
 
     std::list<BamAlignment> readBuffer;
 
-    while(br.GetNextAlignment(al)){
+    int blockId = 0;
+
+    while(reader.GetNextAlignment(al)){
         if(readBuffer.empty()){
             readBuffer.push_back(al);
             continue;
         }
         else{
             if(readBuffer.back().Name != al.Name){
-
-                //orientBlocks();
+                blockId += 1;
+                processBlock(readBuffer, writer, blockId);
                 readBuffer.clear()     ;
                 readBuffer.push_back(al);
             }
+            else{
+                 readBuffer.push_back(al);
+            }
         }
     }
+
+    reader.Close();
+    writer.Close();
 
     return 0;
 }
--- src/annotate.hpp
+++ src/annotate.hpp
@@ -1,6 +1,20 @@
 #ifndef ANNOTATE_H
 #define ANNOTATE_H
 
+#include <list>
+#include <string>
+#include <algorithm>
+
+/* bamtools */
+#include "api/BamWriter.h"
+#include "api/BamMultiReader.h"
+
+/* my code */
+#include "errors.hpp"
+#include "cigar.hpp"
+
+using namespace BamTools;
+
 int annotate(std::string bfName);
 
 #endif
